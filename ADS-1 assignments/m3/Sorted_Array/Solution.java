f3abf27c8b369360c931acd4a8726a2fc9833b1b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-26 21:54:53 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ ADS-1 assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Solution {
+	private Solution() {
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n1 = Integer.parseInt(scan.nextLine());
+		int n2 = Integer.parseInt(scan.nextLine());
+		String[] arr1 = scan.nextLine().split(",");
+		String[] arr2 = scan.nextLine().split(",");
+		Sortedarr array = new Sortedarr(arr1, arr2);
+		System.out.println(array.result()); 
+	}
+}
+class Sortedarr {
+	private String[] arr1;
+	private String[] arr2;
+	private String[] arr;
+	public Sortedarr(String[] arrr1, String[] arrr2) {
+		this.arr1 = arrr1;
+		this.arr2 = arrr2;
+		this.arr = new String[arr1.length * arr2.length];
+	}
+	public String[] result() {
+		int k = 0;
+		int i = 0;
+		int j = 0;
+		int max = 0;
+		while (i < arr1.length && j < arr2.length) {
+			arr[k] = arr1[i];
+			arr[k + 1] = arr2[j];
+			if (Integer.parseInt(arr[k]) > Integer.parseInt(arr[k + 1])) {
+				max = Integer.parseInt(arr[k]);
+				arr[k] = arr[k + 1];
+				arr[k + 1] = String.valueOf(max);
+			}
+			i++;
+			j++;
+			k++;
+		}
+		return arr;
+	}
+}
