fa2c01c5b7a6011fb66e872b6a793f3a7df6d477
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-01 16:16:47 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution {
 	public static void main(String[] args) {
 		Steque steque = new Steque();
 		Scanner sc = new Scanner(System.in);
+		String n = sc.nextLine();
+		while (sc.hasNext()) {
 		String[] line = sc.nextLine().split(" ");
 		switch(line[0]) {
 			case "push":
 			steque.push(Integer.parseInt(line[1]));
+			System.out.println(steque);
 			break;
 			case "pop":
+			try {
 			steque.pop();
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
 			break;
 			case "enqueue":
 			steque.enqueue(Integer.parseInt(line[1]));
+			System.out.println(steque);
 			break;
 			default:
+			steque = new Steque();
 			break;
+		}
+			
 		}
 	}
 }
@@ -33,10 +45,10 @@ class Steque {
 			this.item = item;
 			this.next = null;
 		}
-		Node(int item, Node node) {
-			this.item = item;
-			this.next = node;
-		}
+		// Node(int item, Node nod/e) {
+			// this.item = item;
+			// this.next = node;
+		// }
 	}
 	Steque() {
 		this.head = null;
@@ -46,17 +58,23 @@ class Steque {
 	public boolean isEmpty() {
 		return head == null && tail == null;
 	}
-	public void push(int element) {
-		Node newhead = new Node();
+	public int push(int element) {
+		Node newhead = new Node(element);
 		newhead.next = head;
 		head = newhead;
 		size++;
+		// System.out.println(head.item);
+		return head.item;
 	}
-	public int pop() {
+	public int pop() throws Exception{
+		if (head != null) {
 		int data = head.item;
 		head = head.next;
 		size--;
-		return data;
+		return data;	
+		} else {
+			throw new Exception("Steque is empty.");
+		}
 	}
 	public void enqueue(int element) {
 		Node newhead = new Node(element);
@@ -72,6 +90,20 @@ class Steque {
 		oldtail.next = tail;
 		tail.next = null;
 		size++;
+	}
+	public String toString() {
+		String s = "";
+		if (head == null) {
+			return s;
+		}
+		s = s + head.item;
+		Node temp = head.next;
+		while (temp != null) {
+			s = s + " " + temp.item;
+			temp = temp.next;
+		}
+		return s;
+
 	}
 
 }
