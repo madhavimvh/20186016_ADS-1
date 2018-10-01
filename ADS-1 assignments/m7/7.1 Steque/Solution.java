591e9030b72e7e792ddc9ca6a05b8840076ecdc3
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-01 21:24:56 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -1,112 +1,193 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
-		private int value;
-		private Node next;
-		Node(int v, Node node) {
-			this.value = v;
-			this.next = node;
-		}
-		public int getItem() {
-			return this.value;
-		}
-		public void setItem(int value) {
-			this.value = value;
-		}
-		public Node getNext() {
-			return this.next;
-		}
-		public void setNext(Node node) {
-			this.next = node;
-		}
-		public String toString() {
-			return value + "";
-		}
-	}
+        /**
+         * value is of type int.
+         */
+        private int value;
+        /**
+         * next is of type Node.
+         */
+        private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      v     { parameter_description }
+         * @param      node  The node
+         */
+        Node(final int v, final Node node) {
+            this.value = v;
+            this.next = node;
+        }
+        /**
+         * Gets the item.
+         *
+         * @return     The item.
+         */
+        public int getItem() {
+            return this.value;
+        }
+        /**
+         * Sets the item.
+         *
+         * @param      value  The value
+         */
+        public void setItem(final int value) {
+            this.value = value;
+        }
+        /**
+         * Gets the next.
+         *
+         * @return     The next.
+         */
+        public Node getNext() {
+            return this.next;
+        }
+        /**
+         * Sets the next.
+         *
+         * @param      node  The node
+         */
+        public void setNext(final Node node) {
+            this.next = node;
+        }
+        /**
+         * Returns a string representation of the object.
+         *
+         * @return     String representation of the object.
+         */
+        public String toString() {
+            return value + "";
+        }
+    }
+/**
+ * Class for steque.
+ */
 class Steque {
-	private Node head;
-	private Node tail;
-	private int size;
-	
-	public Steque() {
-		head = null;
-		tail = null;
-		size = 0;
-	}
-	public void push(int element) {
-		Node oldhead = head;
-		head = new Node(element, oldhead);
-		if (oldhead == null) {
-			tail = head;
-		}
-		size++;
-		// System.out.println(element);
-		// System.out.println(head.getItem());
-	}
-	public void pop() {
-		if (head != null && tail != null) {
-			if (head.getNext() == null && tail.getNext() == null) {
-				head = tail = null;
-				size--;
-			} else if (head != null) {
-				head = head.getNext();
-				size--;
-			}
-		}
-	}
-	public void enqueue(int element) {
-		if (tail != null) {
-			Node oldtail = tail;
-			tail = new Node(element, null);
-			oldtail.setNext(tail);
-		} else if (head == null && tail == null) {
-			head = tail = new Node(element, null);
-		}
-		size++;
-	}
-	public String toString() {
-		StringBuffer sb = new StringBuffer("");
-		if (size == 0) {
-			sb.append("Steque is empty.");
-			return sb.toString();
-		} else {
-			Node temp = head;
-			while (temp != null) {
-			sb.append(temp + ", ");
-			temp = temp.getNext();
-			// System.out.println(head.getItem());
-			}
-		return sb.toString().substring(0, sb.length() - 2);
-		}
-	}
+    /**
+     * head is of type Node.
+     */
+    private Node head;
+    /**
+     * tail is of type Node.
+     */
+    private Node tail;
+    /**
+     * size is of type int.
+     */
+    private int size;
+    
+    /**
+     * Constructs the object.
+     */
+    public Steque() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    /**
+     * add an element into stack.
+     *
+     * @param      element  The element
+     */
+    public void push(final int element) {
+        Node oldhead = head;
+        head = new Node(element, oldhead);
+        if (oldhead == null) {
+            tail = head;
+        }
+        size++;
+        // System.out.println(element);
+        // System.out.println(head.getItem());
+    }
+    /**
+     * returns nothing and removes an element f from the stack.
+     */
+    public void pop() {
+        if (head != null && tail != null) {
+            if (head.getNext() == null && tail.getNext() == null) {
+                head = tail = null;
+                size--;
+            } else if (head != null) {
+                head = head.getNext();
+                size--;
+            }
+        }
+    }
+    /**
+     * add an element at the end of stack.
+     *
+     * @param      element  The element
+     */
+    public void enqueue(final int element) {
+        if (tail != null) {
+            Node oldtail = tail;
+            tail = new Node(element, null);
+            oldtail.setNext(tail);
+        } else if (head == null && tail == null) {
+            head = tail = new Node(element, null);
+        }
+        size++;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuffer sb = new StringBuffer("");
+        if (size == 0) {
+            sb.append("Steque is empty.");
+            return sb.toString();
+        } else {
+            Node temp = head;
+            while (temp != null) {
+            sb.append(temp + ", ");
+            temp = temp.getNext();
+            // System.out.println(head.getItem());
+            }
+        return sb.toString().substring(0, sb.length() - 2);
+        }
+    }
 }
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		while (n > 0) {
-		Steque steque = new Steque();
-		while (sc.hasNext()) {
-		String[] line = sc.nextLine().split(" ");
-		switch(line[0]) {
-			case "push":
-			steque.push(Integer.parseInt(line[1]));
-			System.out.println(steque);
-			break;
-			case "pop":
-			steque.pop();
-			System.out.println(steque);
-			break;
-			case "enqueue":
-			steque.enqueue(Integer.parseInt(line[1]));
-			System.out.println(steque);
-			break;
-			default:
-			steque = new Steque();
-			System.out.println();
-			break;
-		}
-		n--;
-		}
-	}
+/**
+ * class for solution.
+ */
+public final class Solution {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        while (n > 0) {
+        Steque steque = new Steque();
+        while (sc.hasNext()) {
+        String[] line = sc.nextLine().split(" ");
+        switch(line[0]) {
+            case "push":
+            steque.push(Integer.parseInt(line[1]));
+            System.out.println(steque);
+            break;
+            case "pop":
+            steque.pop();
+            System.out.println(steque);
+            break;
+            case "enqueue":
+            steque.enqueue(Integer.parseInt(line[1]));
+            System.out.println(steque);
+            break;
+            default:
+            steque = new Steque();
+            System.out.println();
+            break;
+        }
+        n--;
+        }
+    }
 }
 }
