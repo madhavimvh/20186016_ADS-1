94485f230fe983ecc5a4a7d8956b45bc56095ac5
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-02 11:49:44 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -1,38 +1,97 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
-        private int item;
+        /**
+         * value is of type int.
+         */
+        private int value;
+        /**
+         * next is of type Node.
+         */
         private Node next;
-        Node(int item, Node node) {
-            this.item = item;
+        /**
+         * Constructs the object.
+         *
+         * @param      v     { parameter_description }
+         * @param      node  The node
+         */
+        Node(final int v, final Node node) {
+            this.value = v;
             this.next = node;
         }
+        /**
+         * Gets the item.
+         *
+         * @return     The item.
+         */
         public int getItem() {
-            return item;
+            return this.value;
         }
-        public void setItem(int value) {
-            item = value;
+        /**
+         * Sets the item.
+         *
+         * @param      valuee  The value
+         */
+        public void setItem(final int valuee) {
+            this.value = valuee;
         }
+        /**
+         * Gets the next.
+         *
+         * @return     The next.
+         */
         public Node getNext() {
-            return next;
+            return this.next;
         }
-        public void setNext(Node node) {
-            next = node;
+        /**
+         * Sets the next.
+         *
+         * @param      node  The node
+         */
+        public void setNext(final Node node) {
+            this.next = node;
         }
+        /**
+         * Returns a string representation of the object.
+         *
+         * @return     String representation of the object.
+         */
         public String toString() {
-            return item + "";
+            return value + "";
         }
     }
+/**
+ * Class for steque.
+ */
 class Steque {
-    Node head;
-    Node tail;
-    int size;
-    
-    public Steque() {
+    /**
+     * head is of type Node.
+     */
+    private Node head;
+    /**
+     * tail is of type Node.
+     */
+    private Node tail;
+    /**
+     * size is of type int.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     */
+    Steque() {
         head = null;
         tail = null;
         size = 0;
     }
-    public void push(int element) {
+    /**
+     * add an element into stack.
+     *
+     * @param      element  The element
+     */
+    public void push(final int element) {
         Node oldhead = head;
         head = new Node(element, oldhead);
         if (oldhead == null) {
@@ -42,10 +101,14 @@ class Steque {
         // System.out.println(element);
         // System.out.println(head.getItem());
     }
+    /**
+     * returns nothing and removes an element f from the stack.
+     */
     public void pop() {
         if (head != null && tail != null) {
             if (head.getNext() == null && tail.getNext() == null) {
-                head = tail = null;
+                head = null;
+                tail = head;
                 size--;
             } else if (head != null) {
                 head = head.getNext();
@@ -53,16 +116,27 @@ class Steque {
             }
         }
     }
-    public void enqueue(int element) {
+    /**
+     * add an element at the end of stack.
+     *
+     * @param      element  The element
+     */
+    public void enqueue(final int element) {
         if (tail != null) {
             Node oldtail = tail;
             tail = new Node(element, null);
             oldtail.setNext(tail);
         } else if (head == null && tail == null) {
-            head = tail = new Node(element, null);
+            head = new Node(element, null);
+            tail = head;
         }
         size++;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         StringBuffer sb = new StringBuffer("");
         if (size == 0) {
@@ -79,15 +153,28 @@ class Steque {
         }
     }
 }
-public class Solution {
-    public static void main(String[] args) {
+/**
+ * class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         while (n > 0) {
         Steque steque = new Steque();
         while (sc.hasNext()) {
         String[] line = sc.nextLine().split(" ");
-        switch(line[0]) {
+        switch (line[0]) {
             case "push":
             steque.push(Integer.parseInt(line[1]));
             System.out.println(steque);
@@ -110,3 +197,4 @@ public class Solution {
     }
 }
 }
+
