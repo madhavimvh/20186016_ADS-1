5f19b30bcb1e9abf163e3830c4d35220e1087d5b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-08 17:45:27 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1 assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,81 +1,111 @@
 
 import java.util.Scanner;
-
+/**
+ * Class for minheap.
+ *
+ * @param      <E>   { parameter_description }
+ */
 class Minheap<E extends Comparable<E>> {
-	private E[] str;
-	public Minheap(E[] strr) {
-		this.str = strr;
-	}
-	public boolean checkMinheap() {
-		for (int i = 0; i < str.length - 1; i++) {
-			if (!less(str[i], str[i + 1])) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean less(E i, E j) {
-		return i.compareTo(j) <= 0;
-	}
+    /**
+     * str is of type E.
+     */
+    private E[] str;
+    public Minheap(E[] strr) {
+        this.str = strr;
+    }
+    /**
+     * method to check whether the array is Minheap or not.
+     *
+     * @return     returns boolean.
+     */
+    public boolean checkMinheap() {
+        for (int i = 0; i < str.length - 1; i++) {
+            if (!less(str[i], str[i + 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * method to compare two elements.
+     *
+     * @param      i     i is of type E.
+     * @param      j     j is of type E.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean less(final E i, final E j) {
+        return i.compareTo(j) <= 0;
+    }
 }
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		int n = Integer.parseInt(scan.nextLine());
-		switch(str) {
-			case "String":
-			while (n > 0) {
-				String[] st = scan.nextLine().split(",");
-				Minheap<String> heap = new Minheap<String>(st);
-				System.out.println(heap.checkMinheap());
-				n--;
-			}
-			break;
-			case "Integer":
-			while (n > 0) {
-				String[] s = scan.nextLine().split(",");
-				Integer[] sr = new Integer[s.length];
-				for (int i = 0; i < s.length; i++) {
-					sr[i] = Integer.parseInt(s[i]);
-				}
-				Minheap<Integer> heap = new Minheap<Integer>(sr);
-				System.out.println(heap.checkMinheap());
-				n--;
-			}
-			break;
-			case "Double":
- 			while (n > 0) {
-				String[] s = scan.nextLine().split(",");
-				Double[] sr = new Double[s.length];
-				for (int i = 0; i < s.length; i++) {
-					sr[i] = Double.parseDouble(s[i]);
-				}
-				Minheap<Double> heap = new Minheap<Double>(sr);
-				System.out.println(heap.checkMinheap());
-				n--;
-			}
-			break;
-			case "Float":
-			while (n > 0) {
-				String s = scan.nextLine();
-				if (s.equals("")) {
-					System.out.println("false");
-					break;
-				} else {
-				String[] ss = s.split(",");
-				Float[] sr = new Float[ss.length];
-				for (int i = 0; i < ss.length; i++) {
-					sr[i] = Float.parseFloat(ss[i]);
-				}
-				Minheap<Float> heap = new Minheap<Float>(sr);
-				System.out.println(heap.checkMinheap());
-				n--;
-			}
-			}
-			break;
-			default:
-			break;
-		}
-	}
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        int n = Integer.parseInt(scan.nextLine());
+        switch(str) {
+            case "String":
+            while (n > 0) {
+                String[] st = scan.nextLine().split(",");
+                Minheap<String> heap = new Minheap<String>(st);
+                System.out.println(heap.checkMinheap());
+                n--;
+            }
+            break;
+            case "Integer":
+            while (n > 0) {
+                String[] s = scan.nextLine().split(",");
+                Integer[] sr = new Integer[s.length];
+                for (int i = 0; i < s.length; i++) {
+                    sr[i] = Integer.parseInt(s[i]);
+                }
+                Minheap<Integer> heap = new Minheap<Integer>(sr);
+                System.out.println(heap.checkMinheap());
+                n--;
+            }
+            break;
+            case "Double":
+            while (n > 0) {
+                String[] s = scan.nextLine().split(",");
+                Double[] sr = new Double[s.length];
+                for (int i = 0; i < s.length; i++) {
+                    sr[i] = Double.parseDouble(s[i]);
+                }
+                Minheap<Double> heap = new Minheap<Double>(sr);
+                System.out.println(heap.checkMinheap());
+                n--;
+            }
+            break;
+            case "Float":
+            while (n > 0) {
+                String s = scan.nextLine();
+                if (s.equals("")) {
+                    System.out.println("false");
+                    break;
+                } else {
+                String[] ss = s.split(",");
+                Float[] sr = new Float[ss.length];
+                for (int i = 0; i < ss.length; i++) {
+                    sr[i] = Float.parseFloat(ss[i]);
+                }
+                Minheap<Float> heap = new Minheap<Float>(sr);
+                System.out.println(heap.checkMinheap());
+                n--;
+            }
+            }
+            break;
+            default:
+            break;
+        }
+    }
 }
