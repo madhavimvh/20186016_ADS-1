183d4c54b6049abf4446beb13e5aa7c72e5a474f
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-09 17:33:25 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -33,9 +33,11 @@ class MaxPQ {
 	public void insert(int x) {
 		maxpq[++n] = x;
 		swim(n);
+		System.out.println("kjhkj");
 		System.out.println(median(n));
 	}
 	public Double median(int k) {
+		System.out.println("ljlj");
 		if (n == 1) {
 			return Double.parseDouble(String.valueOf(maxpq[1]));
 		} else {
