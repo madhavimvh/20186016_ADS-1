83f8f212426d8052b5e973d5d4dec36ee990c060
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-12 19:20:13 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1 assignments/m17/TaxiCabNumbers/CubeSum.java
+++ ADS-1 assignments/m17/TaxiCabNumbers/CubeSum.java
@@ -11,21 +11,50 @@ public final class CubeSum implements Comparable<CubeSum> {
      * sum is of tyo
      */
     private final int sum;
+    /**
+     * { var_description }.
+     */
     private final int i;
+    /**
+     * { var_description }.
+     */
     private final int j;
+    /**
+     * Constructs the object.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
     public CubeSum(final int i, final int j) {
         this.sum = i*i*i + j*j*j;
         this.i = i;
         this.j = j;
     }
+    /**
+     * { function_description }.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(final CubeSum that) {
         if (this.sum < that.sum) return -1;
         if (this.sum > that.sum) return +1;
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return sum + " = " + i + "^3" + " + " + j + "^3";
     }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         int n = HUN;
         Scanner scan = new Scanner(System.in);
