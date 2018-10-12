e24c950f08c6e90ef90d5b7ae42037eeba281ec4
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-12 19:16:48 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m17/TaxiCabNumbers/CubeSum.java
+++ ADS-1 assignments/m17/TaxiCabNumbers/CubeSum.java
@@ -1,41 +1,44 @@
 import java.util.Scanner;
-public class CubeSum implements Comparable<CubeSum> {
+/**
+ * cubesum class.
+ */
+public final class CubeSum implements Comparable<CubeSum> {
+    /**
+     * 100 is magic number.
+     */
+    private static final int HUN = 600;
+    /**
+     * sum is of tyo
+     */
     private final int sum;
     private final int i;
     private final int j;
-
-    public CubeSum(int i, int j) {
+    public CubeSum(final int i, final int j) {
         this.sum = i*i*i + j*j*j;
         this.i = i;
         this.j = j;
     }
-
-    public int compareTo(CubeSum that) {
+    public int compareTo(final CubeSum that) {
         if (this.sum < that.sum) return -1;
         if (this.sum > that.sum) return +1;
         return 0;
     }
-
     public String toString() {
         return sum + " = " + i + "^3" + " + " + j + "^3";
     }
-
-
-    public static void main(String[] args) {
-        int n = 600;
+    public static void main(final String[] args) {
+        int n = HUN;
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         int M = scan.nextInt();
         int temp = -1;
         int count = 0;
         // int n = Integer.parseInt(arg[0]);
-
         // initialize priority queue
         MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
         for (int i = 0; i <= n; i++) {
             pq.insert(new CubeSum(i, i));
         }
-
         // find smallest sum, print it out, and update
         while (!pq.isEmpty()) {
             CubeSum s = pq.delMin();
@@ -56,7 +59,4 @@ public class CubeSum implements Comparable<CubeSum> {
                 pq.insert(new CubeSum(s.i, s.j + 1));
         }
     }
-
 }
-
- 
