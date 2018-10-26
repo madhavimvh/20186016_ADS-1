87e0d748e95b688b4347ae30a9d850bd0a440848
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-26 15:55:51 +0530

    "ads1_m23_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
+++ ADS-1 assignments/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
@@ -59,9 +59,9 @@ class LinearProbingHashST<Key, Value> {
     }
 
     // hash function for keys - returns value between 0 and M-1
-    private int hash(Key key) {
+    private Integer hash(Key key) {
         // return (key.hashCode() & 0x7fffffff) % m;
-        return (11 * (int) key) % m;
+        return (11 * (Integer) key) % m;
     }
 
     // resizes the hash table to the given capacity by re-hashing all of the keys
