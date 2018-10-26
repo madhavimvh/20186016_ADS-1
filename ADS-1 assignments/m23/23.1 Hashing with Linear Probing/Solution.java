e64939b9c4ebeee7ebb53e4589deb04ef1fec0b2
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-26 17:07:37 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m23/23.1 Hashing with Linear Probing/Solution.java
+++ ADS-1 assignments/m23/23.1 Hashing with Linear Probing/Solution.java
@@ -1,32 +1,45 @@
 import java.util.Scanner;
-
-public class Solution {
-	public static void main(String[] args) {
-		LinearProbingHashST<String, Integer> st = new LinearProbingHashST<String, Integer>();
-		Scanner scan = new Scanner(System.in);
-		String intg = scan.nextLine();
-		while (scan.hasNextLine()) {
-			String[] arr = scan.nextLine().split(" ");
-			switch(arr[0]) {
-				case "put":
-				st.put(arr[1], Integer.parseInt(arr[2]));
-				break;
-				case "display":
-				try {
-				System.out.println(st.display());
-				} catch(Exception ex) {
-					System.out.println("{}");
-				}
-				break;
-				case "delete":
-				st.delete(arr[1]);
-				break;
-				case "get":
-				System.out.println(st.get(arr[1]));
-				break;
-				default:
-				break;
-			}
-		}
-	}
+/**
+ * main method.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LinearProbingHashST<String, Integer> st =
+        new LinearProbingHashST<String, Integer>();
+        Scanner scan = new Scanner(System.in);
+        String intg = scan.nextLine();
+        while(scan.hasNextLine()) {
+            String[] arr = scan.nextLine().split(" ");
+            switch(arr[0]) {
+                case "put":
+                st.put(arr[1], Integer.parseInt(arr[2]));
+                break;
+                case "display":
+                try {
+                System.out.println(st.display());
+                } catch(Exception ex) {
+                    System.out.println("{}");
+                }
+                break;
+                case "delete":
+                st.delete(arr[1]);
+                break;
+                case "get":
+                System.out.println(st.get(arr[1]));
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
