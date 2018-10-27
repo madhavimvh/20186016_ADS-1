a21f8ea09f0eef706731b1457f9b026a68f12087
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-27 12:40:17 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m24/2/Solution.java
+++ ADS-1 assignments/m24/2/Solution.java
@@ -14,6 +14,7 @@ public class Solution {
 			String[] inpp = scan.nextLine().split(" ");
 			if (inpp[0].equals("BE")) {
 				for (Double keys: st.keys()) {
+					System.out.println("khkjh");
 					if (keys <= Double.parseDouble(inpp[1]) && keys >= Double.parseDouble(inpp[2])) {
 						System.out.println(st.get(keys).getName());
 					}
