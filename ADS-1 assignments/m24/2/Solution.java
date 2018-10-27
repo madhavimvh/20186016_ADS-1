82cb28882eb6a0a8f0227249a92b094ba1997b95
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-27 12:21:06 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 assignments/m24/2/Solution.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		RedBlackBST<Double, Studentdata> st = new RedBlackBST<Double, Studentdata>();
+		int inp = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < inp; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			st.put(Double.parseDouble(tokens[2]), new Studentdata(Integer.parseInt(tokens[0]), tokens[1], Double.parseDouble(tokens[2])));
+
+		}
+		int intt = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < intt; i++) {
+			String[] inpp = scan.nextLine().split(" ");
+			if (inpp[0].equals("BE")) {
+				st.keys(Double.parseDouble(inpp[1]), Double.parseDouble(inpp[2]));
+			} 
+			// else if (inpp[0].equals("LE")) {
+
+			// 	}
+			// } else (inpp[0].equals("GE")) {
+			// 	for (int i = inpp[1]; i > 0; i--) {
+					
+			// 	}
+			}
+			
+		}
+	}
+
+class Studentdata {
+	private int rollno;
+	private String name;
+	private double totalmarks;
+	Studentdata(int rollnoo, String namee, double totalmarkss) {
+		rollno = rollnoo;
+		name = namee;
+		totalmarks = totalmarkss;
+	}
+	public String getName() {
+		return name;
+	}
+	// public String getRollno() {
+	// 	return rollno;
+	// }
+	public double getMarks() {
+		return totalmarks;
+	}
+	// public void query(String roll, int n) {
+	// 	if (n == 1) {
+
+	// 	}
+	// }
+
+}
